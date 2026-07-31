708a0503da685ca79dd30b09d9274a9af7f07d9c
wkqrhrqkq <shinyunji3236@gmail.com>
2026-07-31 16:09:57 +0900

    Update page.tsx

--- src/app/[locale]/(marketing)/page.tsx
+++ src/app/[locale]/(marketing)/page.tsx
@@ -1,13 +1,9 @@
 import type { Metadata } from 'next';
 import { getTranslations, setRequestLocale } from 'next-intl/server';
-import { CTA } from '@/templates/CTA';
-import { DemoBanner } from '@/templates/DemoBanner';
-import { FAQ } from '@/templates/FAQ';
 import { Features } from '@/templates/Features';
 import { Footer } from '@/templates/Footer';
 import { Hero } from '@/templates/Hero';
 import { Navbar } from '@/templates/Navbar';
-import { Pricing } from '@/templates/Pricing';
 
 type IndexProps = {
   params: Promise<{ locale: string }>;
@@ -32,13 +28,9 @@ export default async function Index(props: IndexProps) {
 
   return (
     <>
-      <DemoBanner />
       <Navbar />
       <Hero />
       <Features />
-      <Pricing />
-      <FAQ />
-      <CTA />
       <Footer />
     </>
   );
